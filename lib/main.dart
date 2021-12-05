6631d542ef2e8e34be6526ea1c2b899a6ab4e195
Mateus Macedo <mhbassomacedo@gmail.com>
2021-12-05 14:14:31 -0300

    Flutter card and ListTile
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
               radius: 50.0,
@@ -34,49 +35,53 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold),
             ),
-            Container(
-                color: Colors.white,
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100
+              ),
+            ),
+            Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text("+55 01 12345-1234",
-                        style: TextStyle(
-                            color: Colors.teal.shade900,
-                            fontFamily: "SourceSansPro",
-                            fontSize: 20.0))
-                  ],
-                )),
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(Icons.email, color: Colors.teal),
-                  SizedBox(
-                    width: 10.0,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
                   ),
-                  Text(
+                  title: Text("+55 01 12345-1234",
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: "SourceSansPro",
+                          fontSize: 20.0)),
+                )),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.teal),
+                  title: Text(
                     "teste@gmail.com",
                     style: TextStyle(
                         color: Colors.teal.shade900,
                         fontFamily: "SourceSansPro",
                         fontSize: 20.0),
-                  )
-                ],
-              ),
-            )
+                  ),
+                ))
           ],
         )),
       ),
     );
   }
 }
+
+
+// children: <Widget>[
+//                     Icon(
+//                       Icons.phone,
+//                       color: Colors.teal,
+//                     ),
+//                     SizedBox(
+//                       width: 10.0,
+//                     ),
+//                     
+//                   ],
